12e9a13bdd11b19ca3e83a6cfd3f71348aecfe8a
kyoruni <40832190+kyoruni@users.noreply.github.com>
2023-09-21 22:13:00 +0900

    :wrench: リンク設定

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import PermissionView from '../views/Permission.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,6 +17,11 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/permission',
+      name: 'permission',
+      component: PermissionView
     }
   ]
 })
---
+++ src/views/Permission.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="permission">
+    <h1>permission</h1>
+  </div>
+</template>
