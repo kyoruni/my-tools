2828d2d2f04f52f7f3bd1748d4a16c8ed9676cf6
kyoruni <40832190+kyoruni@users.noreply.github.com>
2023-09-22 18:39:11 +0900

    :lipstick: 表示がずれるので、テーブルレイアウトにする

--- src/views/Permission.vue
+++ src/views/Permission.vue
@@ -10,31 +10,69 @@ const other = ref({ read: true, write: true, exec: true });
   <p>group: {{ group }}</p>
   <p>other: {{ other }}</p>
   <div class="permission flex justify-center">
-    <div>
-      <div class="form-control w-52">
-        <label class="label cursor-pointer">
-          <span class="label-text block w-16">所有者</span>
+    <table>
+      <tr class="text-center">
+        <td class="w-16"></td>
+        <td class="w-20">
+          <span class="label-text">読み込み</span>
+        </td>
+        <td class="w-20">
+          <span class="label-text">書き込み</span>
+        </td>
+        <td class="w-20">
+          <span class="label-text">実行</span>
+        </td>
+      </tr>
+      <tr class="text-center">
+        <td class="w-16 label-text text-left">所有者</td>
+        <td class="w-20">
           <input type="checkbox" v-model="owner.read" class="checkbox checkbox-error" />
+        </td>
+        <td class="w-20">
           <input type="checkbox" v-model="owner.write" class="checkbox checkbox-warning" />
+        </td>
+        <td class="w-20">
           <input type="checkbox" v-model="owner.exec" class="checkbox checkbox-info" />
-        </label>
-      </div>
-      <div class="form-control w-52">
-        <label class="label cursor-pointer w-50">
-          <span class="label-text block w-16">グループ</span>
+        </td>
+      </tr>
+      <tr class="text-center">
+        <td class="w-16 label-text text-left">グループ</td>
+        <td class="w-20">
           <input type="checkbox" v-model="group.read" class="checkbox checkbox-error" />
+        </td>
+        <td class="w-20">
           <input type="checkbox" v-model="group.write" class="checkbox checkbox-warning" />
+        </td>
+        <td class="w-20">
           <input type="checkbox" v-model="group.exec" class="checkbox checkbox-info" />
-        </label>
-      </div>
-      <div class="form-control w-52">
-        <label class="label cursor-pointer">
-          <span class="label-text block w-16">その他</span>
+        </td>
+      </tr>
+      <tr class="text-center">
+        <td class="w-16 label-text text-left">その他</td>
+        <td class="w-20">
           <input type="checkbox" v-model="other.read" class="checkbox checkbox-error" />
+        </td>
+        <td class="w-20">
           <input type="checkbox" v-model="other.write" class="checkbox checkbox-warning" />
+        </td>
+        <td class="w-20">
           <input type="checkbox" v-model="other.exec" class="checkbox checkbox-info" />
-        </label>
-      </div>
-    </div>
+        </td>
+      </tr>
+    </table>
   </div>
 </template>
+
+<style scoped>
+#customCheckbox {
+  display: none;
+}
+
+/* 疑似要素を使用してチェックマークを表示 */
+.custom-checkbox::before {
+  content: "\2713";
+  display: inline-block;
+  font-size: 20px; /* フォントサイズを調整 */
+  /* その他のスタイルを追加 */
+}
+</style>
