2bda5d1bf543f1f1ae318dae7c6cc0191ef29415
kyoruni <40832190+kyoruni@users.noreply.github.com>
2023-09-22 18:03:34 +0900

    :lipstick: 見た目作成

--- src/views/Permission.vue
+++ src/views/Permission.vue
@@ -1,5 +1,30 @@
 <template>
-  <div class="permission">
-    <h1>permission</h1>
+  <div class="permission flex justify-center">
+    <div class="hoge">
+      <div class="form-control w-52">
+        <label class="label cursor-pointer">
+          <span class="label-text block w-16">所有者</span>
+          <input type="checkbox" checked="checked" class="checkbox checkbox-error" />
+          <input type="checkbox" checked="checked" class="checkbox checkbox-warning" />
+          <input type="checkbox" checked="checked" class="checkbox checkbox-info" />
+        </label>
+      </div>
+      <div class="form-control w-52">
+        <label class="label cursor-pointer w-50">
+          <span class="label-text block w-16">グループ</span>
+          <input type="checkbox" checked="checked" class="checkbox checkbox-error" />
+          <input type="checkbox" checked="checked" class="checkbox checkbox-warning" />
+          <input type="checkbox" checked="checked" class="checkbox checkbox-info" />
+        </label>
+      </div>
+      <div class="form-control w-52">
+        <label class="label cursor-pointer">
+          <span class="label-text block w-16">その他</span>
+          <input type="checkbox" checked="checked" class="checkbox checkbox-error" />
+          <input type="checkbox" checked="checked" class="checkbox checkbox-warning" />
+          <input type="checkbox" checked="checked" class="checkbox checkbox-info" />
+        </label>
+      </div>
+    </div>
   </div>
 </template>
