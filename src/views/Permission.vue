0741e3d33605cf6ed1c0429d5f799b8f6f5508d6
kyoruni <40832190+kyoruni@users.noreply.github.com>
2023-09-22 19:13:08 +0900

    :wrench: 数値のパーミッションを表示

--- src/views/Permission.vue
+++ src/views/Permission.vue
@@ -1,8 +1,46 @@
 <script setup lang="ts">
-import { ref } from "vue";
-const owner = ref({ read: true, write: true, exec: true });
-const group = ref({ read: true, write: true, exec: true });
-const other = ref({ read: true, write: true, exec: true });
+import { ref, computed } from "vue";
+const READ_NUM = 4;
+const WRITE_NUM = 2;
+const EXEC_NUM = 1;
+const DEFAULT_NUM = 0;
+
+const owner = ref({ read: true, write: true, exec: true, num: 7, sym: "-" });
+const group = ref({ read: true, write: true, exec: true, num: 7, sym: "-" });
+const other = ref({ read: true, write: true, exec: true, num: 7, sym: "-" });
+
+const resultOwnerNum = computed(() => {
+  return "hoge";
+});
+
+const changeOwnerCheck = () => {
+  const readNum = owner.value.read ? READ_NUM : DEFAULT_NUM;
+  const writeNum = owner.value.write ? WRITE_NUM : DEFAULT_NUM;
+  const execNum = owner.value.exec ? EXEC_NUM : DEFAULT_NUM;
+  owner.value.num = readNum + writeNum + execNum;
+};
+
+const changeGroupCheck = () => {
+  const readNum = group.value.read ? READ_NUM : DEFAULT_NUM;
+  const writeNum = group.value.write ? WRITE_NUM : DEFAULT_NUM;
+  const execNum = group.value.exec ? EXEC_NUM : DEFAULT_NUM;
+  group.value.num = readNum + writeNum + execNum;
+};
+
+const changeOtherCheck = () => {
+  const readNum = other.value.read ? READ_NUM : DEFAULT_NUM;
+  const writeNum = other.value.write ? WRITE_NUM : DEFAULT_NUM;
+  const execNum = other.value.exec ? EXEC_NUM : DEFAULT_NUM;
+  other.value.num = readNum + writeNum + execNum;
+};
+
+const resultNum = computed(() => {
+  return owner.value.num.toString() + group.value.num.toString() + other.value.num.toString()
+});
+
+const resultSym = computed(() => {
+  return "記号！";
+});
 </script>
 
 <template>
@@ -26,47 +64,47 @@ const other = ref({ read: true, write: true, exec: true });
       <tr class="text-center">
         <td class="w-16 label-text text-left">所有者</td>
         <td class="w-20">
-          <input type="checkbox" v-model="owner.read" class="checkbox checkbox-error" />
+          <input type="checkbox" v-model="owner.read" class="checkbox checkbox-error" @change="changeOwnerCheck"/>
         </td>
         <td class="w-20">
-          <input type="checkbox" v-model="owner.write" class="checkbox checkbox-error" />
+          <input type="checkbox" v-model="owner.write" class="checkbox checkbox-error" @change="changeOwnerCheck"/>
         </td>
         <td class="w-20">
-          <input type="checkbox" v-model="owner.exec" class="checkbox checkbox-error" />
+          <input type="checkbox" v-model="owner.exec" class="checkbox checkbox-error" @change="changeOwnerCheck"/>
         </td>
       </tr>
       <tr class="text-center">
         <td class="w-16 label-text text-left">グループ</td>
         <td class="w-20">
-          <input type="checkbox" v-model="group.read" class="checkbox checkbox-warning" />
+          <input type="checkbox" v-model="group.read" class="checkbox checkbox-warning" @change="changeGroupCheck" />
         </td>
         <td class="w-20">
-          <input type="checkbox" v-model="group.write" class="checkbox checkbox-warning" />
+          <input type="checkbox" v-model="group.write" class="checkbox checkbox-warning" @change="changeGroupCheck" />
         </td>
         <td class="w-20">
-          <input type="checkbox" v-model="group.exec" class="checkbox checkbox-warning" />
+          <input type="checkbox" v-model="group.exec" class="checkbox checkbox-warning" @change="changeGroupCheck" />
         </td>
       </tr>
       <tr class="text-center">
         <td class="w-16 label-text text-left">その他</td>
         <td class="w-20">
-          <input type="checkbox" v-model="other.read" class="checkbox checkbox-info" />
+          <input type="checkbox" v-model="other.read" class="checkbox checkbox-info" @change="changeOtherCheck" />
         </td>
         <td class="w-20">
-          <input type="checkbox" v-model="other.write" class="checkbox checkbox-info" />
+          <input type="checkbox" v-model="other.write" class="checkbox checkbox-info" @change="changeOtherCheck" />
         </td>
         <td class="w-20">
-          <input type="checkbox" v-model="other.exec" class="checkbox checkbox-info" />
+          <input type="checkbox" v-model="other.exec" class="checkbox checkbox-info" @change="changeOtherCheck" />
         </td>
       </tr>
     </table>
   </div>
   <div class="text-center mt-4">
     <span class="label-text inline-block w-16">数値</span>
-    <input type="text" placeholder="数値" class="input input-bordered w-20 max-w-xs" />
+    <input type="text" v-model="resultNum" class="input input-bordered w-20 max-w-xs" />
   </div>
   <div class="text-center mt-4">
     <span class="label-text inline-block w-16">記号</span>
-    <input type="text" placeholder="記号" class="input input-bordered w-20 max-w-xs" />
+    <input type="text" v-model="resultSym" class="input input-bordered w-20 max-w-xs" />
   </div>
 </template>
